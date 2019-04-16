c299e35f45b897a9245fb639c1ef328866af350e
dannyp99 <dannypires99@gmail.com>
2019-04-16 11:21:33 -0400

    First argument is now the number of trips followed by their mileage

--- driving.c
+++ driving.c
@@ -4,23 +4,28 @@
 
 float total = 0.0;
 pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
-
+int trips =0;
 void *miles(void* par){
 	char *cpar = (char*) par;
-	int miles = atoi(cpar);
+	float miles = atof(cpar);
 	pthread_mutex_lock(&m1);
-	total = miles*(.58)*9;
+	total = miles*(.58)*trips;
 	printf("The total for this trip is: %.2f\n",total);		
 	pthread_mutex_unlock(&m1);
 	return NULL;
 }
 
 int main(int argc, char **argv){
-	pthread_t tid[argc];
-	for(int i =0; i<argc-1;i++){
-		pthread_create(&tid[i],NULL, (void*)miles, argv[i+1]);	
+	if(argc <3){
+		printf("Please pass the following: ./driving <Number of trips> <Number of Miles>\n");
+		return 0;
 	}
-	for(int i = 0; i<argc-1;i++){
+	pthread_t tid[argc-1];
+	trips = atoi(argv[1]);
+	for(int i =0; i<argc-2;i++){
+		pthread_create(&tid[i],NULL, (void*)miles, argv[i+2]);	
+	}
+	for(int i = 0; i<argc-2;i++){
 		pthread_join(tid[i],NULL);
 	}
 	pthread_exit(0);
