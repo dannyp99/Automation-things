bde5c50ceae297dae4953b085739c92b7eaae542
dannyp99 <dannypires99@gmail.com>
2019-04-19 09:25:38 -0400

    Complete commenting

--- driving.c
+++ driving.c
@@ -7,11 +7,11 @@ pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
 int trips =0.0;
 float rate = 0.0;
 void *miles(void* par){
-	float miles = atof((char*) par);
-	pthread_mutex_lock(&m1);
-	total = miles*(rate)*trips;
-	printf("The total for this trip is: %.2f\n",total);		
-	pthread_mutex_unlock(&m1);
+	float miles = atof((char*) par);//convert the param to the float needed.
+	pthread_mutex_lock(&m1);//Lock the critical section
+	total = miles*(rate)*trips;//Calculate the cost.
+	printf("The total for this trip is: %.2f\n",total);//Print the total		
+	pthread_mutex_unlock(&m1);//Unlock now that the critical section is clear
 	return NULL;
 }
 
@@ -24,10 +24,10 @@ int main(int argc, char **argv){
 	trips = atoi(argv[1]); //pass and get the number of trips
 	rate  = atof(argv[2]); // pass and get the rate
 	for(int i =0; i<argc-3;i++){
-		pthread_create(&tid[i],NULL, (void*)miles, argv[i+3]);	
+		pthread_create(&tid[i],NULL, (void*)miles, argv[i+3]);//Create the threads	
 	}
 	for(int i = 0; i<argc-3;i++){
-		pthread_join(tid[i],NULL);
+		pthread_join(tid[i],NULL); // Get all of them back
 	}
 	pthread_exit(0);
 }
