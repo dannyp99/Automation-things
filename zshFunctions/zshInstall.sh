6e01298530fdfb55bc99001e7212f7f7a91a33a1
Ubuntu <ubuntu@ip-172-31-54-164.ec2.internal>
2020-06-10 04:50:32 +0000

    further test needed major cleanup completed and tested

--- zshFunctions/zshInstall.sh
+++ zshFunctions/zshInstall.sh
@@ -1,48 +1,54 @@
 #!/bin/bash
 if [[ $1 = "-h" ]];then
 	echo "-h 	help"
+	echo "--font-manager	used if you need to install font manager"
+	echo "--install-font	open the downloaded font file to install to system"
 	exit
 fi
 
 if [[ -f "/usr/bin/zsh" ]] || [[ -f "/bin/zsh" ]];then 
 	echo "zsh is already installed"
 else 
-	sudo apt install zsh
+	sudo apt -y install zsh
 fi
 
-if [[  -d "$HOME/.oh-my-zsh" ]];then
+if [[ -d "$HOME/.oh-my-zsh" ]];then
 	echo "oh-my-zsh is already installed"
 else
 	bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-if [[ -f "$ZSH_CUSTOM/themes/powerlevel10k" ]];then
+if [[ -d "$HOME/.oh-my-zsh/themes/powerlevel10k" ]];then
 	echo "powerlevel10k is already installed"
 else
 	sudo git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/themes/powerlevel10k
 fi
 
 if [[ -f "/usr/share/fonts/Fira\ Mono\ Regular\ Nerd\ Font\ Complete.otf" ]] || [[ -f "/usr/local/share/fonts/Fira\ Mono\ Regular\ Nerd\ Font\ Complete.otf" ]] || [[ -f "$HOME/.fonts/Fira\ Mono\ Regular\ Nerd\ Font\ Complete.otf" ]];then
-	echo "Fonts installed"
+	echo "Fonts installed already"
 else
 	if [[ ! -f "/bin/wget" ]];then
-		sudo apt install wget
+		sudo apt -y install wget
 	fi
-	if [[ ! -f "$HOME/Downloads/Fira\ Mono\ Regular\ Nerd\ Font\ Complete.otf" ]];then
+	if [[ ! -f "$HOME/Downloads/Fira\ Mono\ Regular\ Nerd\ Font\ Complete.otf" ]] && { [[ $1 = "--font-manager" ]] || [[ $1 = "--install-font" ]]; };then
 		wget -O $HOME/Downloads/Fira\ Mono\ Regular\ Nerd\ Font\ Complete.otf https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraMono/Regular/complete/Fira%20Mono%20Regular%20Nerd%20Font%20Complete.otf?raw=true
 	fi
-	sudo apt install font-manager
-	font-manager $HOME/Downloads/Fura\ Mono\ Regular\ Nerd\ Font\ Complete.otf
+	if [[ $1 = "--font-manager" ]];then
+		sudo apt -y install font-manager
+		font-manager $HOME/Downloads/Fura\ Mono\ Regular\ Nerd\ Font\ Complete.otf
+	elif [[ $1 = "--install-font" ]];then
+		open $HOME/Downloads/Fura\ Mono\ Regular\ Nerd\ Font\ Complete.otf
+	fi
 fi
 
-if [[ -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]];then
-	echo "plugin installed"
+if [[ -d "$HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting" ]] && [[ -d "$HOME/.oh-my-zsh/plugins/zsh-autosuggestions" ]];then
+	echo "plugin already installed"
 else 
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
 	git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
 	cp $HOME/.zshrc zshrc_backup
 	echo "backup created in current directory as zshrc_backup"
-	sed 's/ZSH_THEME.*/ZSH_THEME="powerlevel10k\/powerlevel10k"|POWERLEVEL9K_MODE="nerdfont-complete"/g' zshrc_backup | tr '|' '\n' > $HOME/.zshrc
+	sed 's/ZSH_THEME.*/ZSH_THEME="powerlevel10k\/powerlevel10k"%POWERLEVEL9K_MODE="nerdfont-complete"/g' zshrc_backup | tr '%' '\n' > $HOME/.zshrc
 	sed -i 's/plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' $HOME/.zshrc
 fi
 echo "All Done Please refresh your your terminal or open zsh and type 'source ~/.zshrc'"
--- zshFunctions/zshUninstall.sh
+++ zshFunctions/zshUninstall.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-if [[ "$1" = "--remove-all" ]];then
-	sudo apt purge wget
-fi
 if [[ "$1" = "-h" ]];then
 	echo "-h 	help"
 	echo "--remove-all	remove all programs added through the install this includes wget"
 	echo "default running will remove oh-my-zsh as well as zsh"
+	exit
 fi
+
+if [[ "$1" = "--remove-all" ]];then
+	sudo apt -y purge wget
+fi
+
+if [[ -f "/usr/bin/font-manager" ]];then
+	sudo apt -y purge font-manager
+fi
+
 if [[ "$SHELL" = "/bin/zsh" ]];then
 	echo "Can't unsinstall zsh, zsh is still the default shell"
 	echo "Please use chsh command to change your shell"
 else
-	sudo apt purge zsh
+	sudo apt -y purge zsh
 	rm $HOME/.zshrc
 	rm $HOME/.zsh_history
 fi
@@ -22,4 +29,4 @@ else
 	echo "oh-my-zsh is already deleted"
 fi
 
-sudo apt autoremove
+sudo apt -y autoremove
